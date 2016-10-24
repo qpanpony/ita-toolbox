2e44e5b501abf852ba6d62be500d4d6955187f58
Florian Pausch <fpa@akustik.rwth-aachen.de>
2016-10-24 10:51:23 +0200

    removed bug in ita_pause.m

--- kernel/StandardRoutines/ita_pause_ns.m
+++ kernel/StandardRoutines/ita_pause_ns.m
@@ -1,4 +1,4 @@
-function [] = ita_pause_ns( nano_seconds )
+function ita_pause_ns( nano_seconds )
 % ita_pause_ns pauses (sleeps) for given time (units in nanoseconds)
     java.util.concurrent.locks.LockSupport.parkNanos( nano_seconds );
 end
--- kernel/StandardRoutines/ita_pause_us.m
+++ kernel/StandardRoutines/ita_pause_us.m
@@ -1,4 +1,4 @@
-function [] = ita_pause_us( micro_seconds )
+function ita_pause_us( micro_seconds )
 % ita_pause_us pauses (sleeps) for given time (units in micro seconds)
     java.util.concurrent.locks.LockSupport.parkNanos( micro_seconds * 1e3 );
 end
