6fd09c86fe77458ecf458b0a9bd1b1b496436bf5
Jan-Gerrit Richter <jri@akustik.rwth-aachen.de>
2017-01-17 17:18:09 +0100

    added plot_comet3 to itacoordinates

---
+++ kernel/ClassStuff/@itaCoordinates/comet3.m
@@ -0,0 +1,20 @@
+function [ varargout ] = comet3(this,varargin )
+
+% <ITA-Toolbox>
+% This file is part of the ITA-Toolbox. Some rights reserved. 
+% You can find the license for this m-file in the license.txt file in the ITA-Toolbox folder. 
+% </ITA-Toolbox>
+
+hFig = PlotComet_3D(this.x, this.y, this.z, varargin{:});
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+axis equal vis3d
+if nargout
+   varargout = {hFig};
+else
+    varargout = {};
+end
+
+end
+
