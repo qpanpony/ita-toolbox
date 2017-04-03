ff385f691cb50c2400b7bf74f2346bf1ba7b3e62
Jonas Stienen <jst@akustik.rwth-aachen.de>
2017-04-03 20:49:48 +0200

    Little better init of virtual sound sources and listeners in simplest VA scene

--- applications/VirtualAcoustics/VA/itaVA_simplest_scene.m
+++ applications/VirtualAcoustics/VA/itaVA_simplest_scene.m
@@ -6,10 +6,10 @@ ita_write_wav( ita_demosound, 'ita_demosound.wav', 'overwrite' );
 X = va.createAudiofileSignalSource( 'ita_demosound.wav' );
 va.setAudiofileSignalSourcePlaybackAction( X, 'play' )
 va.setAudiofileSignalSourceIsLooping( X, true );
-S = va.createSoundSource( 'itaVA_Source' );
+S = va.createSoundSource( 'itaVA_Source', 'all' );
 va.setSoundSourcePosition( S, [ 0 1.7 -2 ] );
 va.setSoundSourceSignalSource( S, X );
 H = va.loadHRIRDataset( '$(DefaultHRIR)' );
-L = va.createListener( 'itaVA_Listener', 'default', H );
+L = va.createListener( 'itaVA_Listener', 'all', H );
 va.setListenerPosition( L, [ 0 1.7 0 ] );
 va.setActiveListener( L );
