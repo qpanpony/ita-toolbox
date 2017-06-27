c25b837f783bb96d97446d1485a889caec77b985
Michael Kohnen <michael.kohnen@akustik.rwth-aachen.de>
2017-06-27 14:36:57 +0200

    Updated loudspeaker positions

--- applications/SpatialAudio/VR_loudspeaker_position.m
+++ applications/SpatialAudio/VR_loudspeaker_position.m
@@ -10,35 +10,29 @@ function pos = VR_loudspeaker_position(varargin)
 opts.virtualSpeaker     = false;            % true      || Indicates whether to add virtual speaker achieve a more regular distribution (stabilizes the pseudoinverse in Ambisonics Decoding)
 opts.coordSystem        = 'itaCoordinates'; % 'openGL'  || indicates in which coordinate system the output is
 opts.isItaCoordinates   = false;            % true      || indicates whether the output is a itaCoordinate or not (warning: if combined with coordSystem='opneGL', the angles for azimuth and elevation are not correct
-opts.heightCorrection   = -1.3275;          % in meter  || ensures that the loudspeakers are around the point (0 0 0), standard value is height of the bigger loudspeaker in the horizontal plane
+opts.heightCorrection   = 0.0597;          % in meter  || ensures that the loudspeakers are around the point (0 0 0), standard value is height of the bigger loudspeaker in the horizontal plane
 
 opts=ita_parse_arguments(opts, varargin);
 
 hc=opts.heightCorrection;
 % X     Y     Z    (openGL)
-pos = [...
-    -2  1.3275+hc   -2;...
-    2  1.3275+hc   -2;...
-    2  1.3275+hc    2;...
-    -2  1.3275+hc    2;...
-    0  2.6+hc      -3;... % ls5
-    3  2.6+hc       0;...
-    0  2.6+hc       3;...
-    -3  2.6+hc       0;...
-    0  0.2+hc      -3;...
-    3  0.2+hc       0;... % ls10
-    0  0.2+hc       3;...
-    -3  0.2+hc       0;];
+pos = itaCoordinates(zeros(12,3));
+pos.r= 2.28;
+pos.phi_deg=[45 315 225 135 0 270 180 90 0 270 180 90];
+pos.theta_deg=[88.5 88.5 88.5 88.5 60 60 60 60 118 118 118 118];
+    
+pos.z=pos.z-hc;
+
 if opts.virtualSpeaker
     pos = [pos;...
         0  0+hc  0;... % virtual speaker
         0  3+hc  0];   % virtual speaker
 end
 
-if strcmp(opts.coordSystem,'itaCoordinates')
-    pos = ita_openGL2Matlab(pos);
+if strcmp(opts.coordSystem,'openGL')
+    pos = ita_matlab2openGL(pos);
 end
 
-if opts.isItaCoordinates
-    pos = itaCoordinates(pos);
+if (~opts.isItaCoordinates)
+    pos = pos.cart;
 end
