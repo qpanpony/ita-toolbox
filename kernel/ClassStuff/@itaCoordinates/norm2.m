8c882fb6c0b5e0a4ec53b7a734ea872787286474
Jan-Gerrit Richter <jri@akustik.rwth-aachen.de>
2017-12-02 14:40:15 +0100

    added norm2 to itaCoordinates

---
+++ kernel/ClassStuff/@itaCoordinates/norm2.m
@@ -0,0 +1,11 @@
+function d = norm2(v1)
+% calculate the length of vectors
+
+% <ITA-Toolbox>
+% This file is part of the ITA-Toolbox. Some rights reserved. 
+% You can find the license for this m-file in the license.txt file in the ITA-Toolbox folder. 
+% </ITA-Toolbox>
+
+
+d = sqrt(v1.x.^2+v1.y.^2+v1.z.^2);
+end %eof
