743096db1a87ac82da64193a5e0def30c99c9283
Dipl.-Ing. Jonas Stienen <jst@akustik.rwth-aachen.de>
2018-05-27 20:44:07 +0200

    Improving generic unity / dirac daff data functions and adding more examples how to use them

--- applications/VirtualAcoustics/openDAFF/OpenDAFFv1.7/datafuncs/dfUnityMS.m
+++ applications/VirtualAcoustics/openDAFF/OpenDAFFv1.7/datafuncs/dfUnityMS.m
@@ -1,13 +1,19 @@
-function [ freqs, mags, metadata ] = dfUnityMS( ~, ~, ~ )
+function [ freqs, mags, metadata ] = dfUnityMS( ~, ~, unity_ms_config )
 % Omnidirectional magnitude spectrum
            
-    % Third-octave resolution
-    freqs = [20 25 31.5 40 50 63 80 100 125 160 ...
-             200 250 315 400 500 630 800 1000 1250 1600 ...
-             2000 2500 3150 4000 5000 6300 8000 10000 12500 16000 20000];
+    if ~isempty( fieldnames( unity_ms_config ) )
+        freqs = unity_ms_config.freqs;
+        channels = unity_ms_config.channels;
+    else
+        % Third-octave resolution
+        freqs = [20 25 31.5 40 50 63 80 100 125 160 ...
+                 200 250 315 400 500 630 800 1000 1250 1600 ...
+                 2000 2500 3150 4000 5000 6300 8000 10000 12500 16000 20000];
+
+        channels = 1;
+    end
     
-    channels = 1;
-    mags = zeros(channels, length(freqs));
+    mags = zeros( channels, numel( freqs ) );
     metadata = [];
     
     for c=1:channels
