c0a56142ee173fb6200ebbc45d9475b5032fa72d
Philipp Schäfer <Philipp.marco.schaefer@rwth-aachen.de>
2018-10-10 14:33:54 +0200

    itaComsolModel -added documentation -now stores currently used objects for geometry, physics, mesh and study 	-are used as default nodes when changing the model -create functions now check whether object with given tag already exists -added get functions for mesh nodes

--- applications/Numeric/Comsol/@itaComsolModel/itaComsolModel.m
+++ applications/Numeric/Comsol/@itaComsolModel/itaComsolModel.m
@@ -1,18 +1,49 @@
 classdef itaComsolModel < handle
-    %itaComsolModel Summary of this class goes here
-    %   Detailed explanation goes here
+    %itaComsolModel Interface to adjust work with comsol model
+    %   This class takes an existing comsol model and provides interfaces
+    %   to adjust certain parameters such as boundary conditions
+    %   (impedances) and sources. Also provides the function to run a
+    %   simulation and gather results in ita-formats.
+    %   
+    %   Note, that it is crucial to define the basis of the comsol model in
+    %   Comsol itself. This includes:
+    %   -Geometry
+    %   -Materials
+    %   -Physics
+    %       -especially, impedances at boundaries
+    %   -Mesh
+    %   -Study
+    %
+    %   This class is able to create/adjust:
+    %   -Global Definitions
+    %       -Interpolations
+    %   -Geometry
+    %       -for sources (points / boundary surfaces)
+    %
+    %   This class is able to adjust:
+    %   -Materials
+    %       -...(to be added in future)
+    %   -Physics
+    %       -frequency dependent values for boundary impedances
+    %       -frequency dependent source parameters (velocity / pressure)
+    %   -Mesh
+    %       -... ()
+    %   -Study
+    %       -frequency vector
     
     properties(Access = private)
-        mModel;
+        mModel;         %Comsol model (com.comsol.clientapi.impl.ModelClient)
         
         mCurrentGeometry;
         mCurrentPhysics;
+        mCurrentMesh;
         mCurrentStudy;
     end
     
     properties(Dependent = true)
         currentGeometry;%Currently used geometry node. Geometry based operations are processed on this node if not specified differently.
         currentPhysics; %Currently used physics node. Physics based operations are processed on this node if not specified differently.
+        currentMesh;    %Currently used mesh node. Mesh based operations are processed on this node if not specified differently.
         currentStudy;   %Currently used study node. Study based operations are processed on this node if not specified differently.
     end
     
@@ -23,8 +54,27 @@ classdef itaComsolModel < handle
         function out = get.currentPhysics(obj)
             out = obj.mCurrentPhysics;
         end
+        function out = get.currentMesh(obj)
+            out = obj.mCurrentMesh;
+        end
         function out = get.currentStudy(obj)
             out = obj.mCurrentStudy;
+        end
+        function set.currentGeometry(obj, input)
+            assert(isa(input, 'com.comsol.clientapi.impl.GeomSequenceClient'), 'Input must be a Comsol geometry node')
+            obj.mCurrentGeometry = input;
+        end
+        function set.currentPhysics(obj, input)
+            assert(isa(input, 'com.comsol.clientapi.impl.PhysicsClient'), 'Input must be a Comsol physics node')
+            obj.mCurrentPhysics = input;
+        end
+        function set.currentMesh(obj, input)
+            assert(isa(input, 'com.comsol.clientapi.impl.MeshSequenceClient'), 'Input must be a Comsol mesh node')
+            obj.mCurrentMesh = input;
+        end
+        function set.currentStudy(obj, input)
+            assert(isa(input, 'com.comsol.clientapi.impl.StudyClient'), 'Input must be a Comsol study node')
+            obj.mCurrentStudy = input;
         end
     end
     
@@ -39,16 +89,22 @@ classdef itaComsolModel < handle
             
             obj.mCurrentGeometry = obj.FirstGeometry();
             obj.mCurrentPhysics = obj.FirstPressureAcoustics();
+            obj.mCurrentMesh = obj.FirstMesh();
             obj.mCurrentStudy = obj.FirstStudy();
+            
+            assert(isempty(obj.mCurrentGeometry), 'No Comsol geometry node found')
+            assert(isempty(obj.mCurrentPhysics), 'No Comsol physics node found')
+            assert(isempty(obj.mCurrentMesh), 'No Comsol mesh node found')
+            assert(isempty(obj.mCurrentStudy), 'No Comsol study node found')
         end
     end
     
     %% Global Definitions
     methods
         function interpolationNode = CreateInterpolation(obj, functionName, propertyStruct)
-            %Create an Interpolation with the given function name.
-            %Optionally, the function properties can be set using a
-            %property struct.
+            %Creates an Interpolation with the given function name (if not
+            %already existing) and returns it. Optionally, the function
+            %properties can be set using a property struct.
             %   The function name must be a valid Matlab variable name (no
             %   whitespace, not starting with a number and so on...).
             %   The property struct has the property names as field names
@@ -57,17 +113,19 @@ classdef itaComsolModel < handle
             assert(isvarname(functionName), 'First input must be a valid variable name')
             if nargin > 2; assert(isstruct(propertyStruct), 'Second input must be a struct with function properties'); end
             
-            obj.mModel.func.create(functionName, 'Interpolation')
-            obj.mModel.func(functionName).label(functionName);
+            if ~obj.hasChildNode(obj.mModel.func, functionName)
+                obj.mModel.func.create(functionName, 'Interpolation');
+            end
             
+            obj.mModel.func(functionName).label(functionName);
             interpolationNode = obj.mModel.func(functionName);
             
             if nargin == 2; return; end
-            obj.setNodeProperties( interpolationNode, propertyStruct )
+            obj.setNodeProperties( interpolationNode, propertyStruct );
         end
     end
     methods(Access = private)
-        function [realInterpolationNode, imagInterpolationNode] = setComplexInterpolation(obj, interpolationBaseName, freqVector, complexDataVector, functionUnits)
+        function [realInterpolationNode, imagInterpolationNode] = createComplexInterpolation(obj, interpolationBaseName, freqVector, complexDataVector, functionUnits)
             %Creates or adjusts two Comsol Interpolation nodes, one for the
             %real and one for the imaginary data and returns the two
             %interpolation nodes.
@@ -78,15 +136,9 @@ classdef itaComsolModel < handle
             %   "linear" extrapolation.
             interpolationNameReal = [interpolationBaseName '_real'];
             interpolationNameImag = [interpolationBaseName '_imag'];
-
-            if ~obj.hasChildNode(obj.mModel.func, interpolationNameReal)
-                obj.CreateInterpolation(interpolationNameReal);
-            end
-            if ~obj.hasChildNode(obj.mModel.func, interpolationNameImag)
-                obj.CreateInterpolation(interpolationNameImag);
-            end
-            realInterpolationNode = obj.mModel.func(interpolationNameReal);
-            imagInterpolationNode = obj.mModel.func(interpolationNameImag);
+            
+            realInterpolationNode = obj.CreateInterpolation(interpolationNameReal);
+            imagInterpolationNode = obj.CreateInterpolation(interpolationNameImag);
             
             propertyStruct.source = 'table';
             propertyStruct.argunit = 'Hz';
@@ -103,6 +155,7 @@ classdef itaComsolModel < handle
     end
     methods(Access = private, Static = true)
         function setInterpolationTableData(interpolationNode, argumentVector, functionVector)
+            assert(isreal(argumentVector) && isreal(functionVector), 'Data vectors for Comsol interpolation must be real valued')
             if isrow(argumentVector); argumentVector = argumentVector.'; end
             if isrow(functionVector); functionVector = functionVector.'; end
             
@@ -124,20 +177,28 @@ classdef itaComsolModel < handle
         end
     end
     methods(Static = true)
-        function setPoint(geomNode, pointName, coords)
+        function [pointNode, selectionTag] = createPointWithSelection(geomNode, pointTag, coords)
+            pointNode = itaComsolModel.createPoint(geomNode, pointTag, coords);            
+            pointNode.set('selresult', true);
+            pointNode.set('selresultshow', 'pnt');
+            
+            selectionTag = [char(geomNode.tag) '_' pointTag '_pnt'];
+        end
+        function pointNode = createPoint(geomNode, pointTag, coords)
             assert(isa(geomNode, 'com.comsol.clientapi.impl.GeomSequenceClient'), 'First input must be a Comsol geometry node')
             assert( (isnumeric(coords) && isvector(coords) && numel(coords)==3) ||...
                 (isa(coords, 'itaCoordinates') && coords.nPoints==1), 'Third input must be a cartesian vector or a single itaCoordinates');
             
-            if ~itaComsolModel.hasFeatureNode(geomNode, pointName)
-                geomNode.create(pointName, 'Point');
-                geomNode.feature(pointName).label(pointName);
+            if ~itaComsolModel.hasFeatureNode(geomNode, pointTag)
+                geomNode.create(pointTag, 'Point');
+                geomNode.feature(pointTag).label(pointTag);
             end
             
             if isa(coords, 'itaCoordinates'); coords = coords.cart; end
             if isrow(coords); coords = coords.'; end
             
-            geomNode.feature(pointName).set('p', coords);
+            pointNode = geomNode.feature(pointTag);
+            pointNode.set('p', coords);
             geomNode.run();
         end
         
@@ -216,7 +277,7 @@ classdef itaComsolModel < handle
             idxPressureAcoustics = false(size(physics));
             for idxPhysics = 1:numel(physics)
                 idxPressureAcoustics(idxPhysics) =...
-                    contains(physics{idxPhysics}.getType, 'PressureAcoustics');
+                    contains(char( physics{idxPhysics}.getType ), 'PressureAcoustics');
             end
             pressureAcoustics = physics(idxPressureAcoustics);
         end
@@ -230,30 +291,44 @@ classdef itaComsolModel < handle
             end
         end
     end
+    methods(Static=true)
+        function bool = IsBoundaryMethod(physics)
+            assert(isa(physics, 'com.comsol.clientapi.physics.impl.PhysicsClient'), 'Input must be a Comsol physics node')
+            bool = strcmp(physics.getType, 'PressureAcousticsBoundaryElements') ||...
+                strcmp(physics.getType, 'BoundaryModeAcoustics');
+        end
+    end
     %------Impedance-------------------------------------------------------
     methods
         function impedanceNodes = ImpedanceNodes(obj, physics)
             %Returns the impedance nodes for the given physics node. If the
             %physics node is left empty or not passed the default one will
             %be used
-            if nargin == 1 || isempty(physics)
-                physics = obj.FirstPhysics();
-                assert(isempty(physics), 'No default physics node found')
+            if nargin == 1
+                physics = obj.mCurrentPhysics;
+                assert(~isempty(physics), 'No default physics node specified')
             else
                 assert(isa(physics, 'com.comsol.clientapi.physics.impl.PhysicsClient'), 'Input must be a Comsol Physics node')
             end
             impedanceNodes = obj.getChildNodesByType(physics, 'Impedance');
         end
         
-        function SetBoundaryGroupImpedance(obj, physics, boundaryGroupName, data)
-            %Sets the data impedance node for the given boundary group name
+        function SetBoundaryGroupImpedance(obj, boundaryGroupName, data, physics)
+            %Sets the data of the impedance node for the given boundary group
             %(=label of a 2D selection in Comsol). The data can either be
             %passed using an itaMaterial or an itaResult/itaAudio.
-            assert(isa(physics, 'com.comsol.clientapi.physics.impl.PhysicsClient'), 'First input must be a Comsol Physics node')
-            assert(ischar(boundaryGroupName) && isrow(boundaryGroupName), 'Second input must be a char row vector')
+            %   Optionally, a physics node can be passed, if you do not
+            %   want to work on currentPhysics
+            if nargin == 3
+                physics = obj.mCurrentPhysics;
+                assert(~isempty(physics), 'No default physics node specified');
+            end
+            
+            assert(ischar(boundaryGroupName) && isrow(boundaryGroupName), 'First input must be a char row vector')
             if isa(data, 'itaMaterial'); data = data.impedance; end
             assert(isa(data, 'itaSuper') && numel(data) == 1 && data.nChannels == 1,...
-                'Third input must either be an itaMaterial with a valid impedance or a single itaSuper')
+                'Second input must either be an itaMaterial with a valid impedance or a single itaSuper')
+            assert(isa(physics, 'com.comsol.clientapi.physics.impl.PhysicsClient'), 'First input must be a Comsol Physics node')
             
             freqVector = data.freqVector;
             freqData = data.freqData;
@@ -270,7 +345,6 @@ classdef itaComsolModel < handle
             %Returns the impedance node of the given physics node that is
             %connected to given boundary group (= Comsol selection). Returns
             %an empty object if no impedance is connected to the given selection.
-            %   The physics node can be left empty to use the default one
             assert(ischar(boundaryGroupName) && isrow(boundaryGroupName), 'Second input must be a char row vector')
             impedanceNodeOfBoundary = [];
             
@@ -292,37 +366,71 @@ classdef itaComsolModel < handle
             %    isnumeric(functionVector) && isvector(functionVector), 'Data vectors must be numeric')
             %assert( numel(argumentVector) == numel(functionVector), 'Both data vectors must be of same length')
             
-            [realInterpolationNode, imagInterpolationNode] = obj.setImpedanceInterpolation(...
+            [realInterpolationNode, imagInterpolationNode] = obj.createImpedanceInterpolation(...
                 interpolationBaseName, freqVector, complexDataVector);
             
             realFuncName = char(realInterpolationNode.tag);
             imagFuncName = char(imagInterpolationNode.tag);
             impedanceExpression = [realFuncName '(freq) + i*' imagFuncName '(freq)'];
+            impedanceNode.set('ImpedanceModel', 'UserDefined');
             impedanceNode.set('Zi', impedanceExpression);
         end
-        function [realInterpolationNode, imagInterpolationNode] = setImpedanceInterpolation(obj, interpolationBaseName, freqVector, complexDataVector)
+        function [realInterpolationNode, imagInterpolationNode] = createImpedanceInterpolation(obj, interpolationBaseName, freqVector, complexDataVector)
             %Creates or adjusts two Comsol Interpolation nodes, one for the
             %real and one for the imaginary impedance data and returns the
             %two interpolation nodes.
-            [realInterpolationNode, imagInterpolationNode] = obj.setComplexInterpolation(interpolationBaseName, freqVector, complexDataVector, 'Pa / m * s');
+            [realInterpolationNode, imagInterpolationNode] = obj.createComplexInterpolation(interpolationBaseName, freqVector, complexDataVector, 'Pa / m * s');
         end
     end
     %------Sources---------------------------------------------------------
     methods
-        function CreatePointSource(obj, physics, source)
+        function CreatePointSource(obj, source, physics)
+            %Creates a point source in physics given an itaSource
+            %   Optionally, a physics node can be passed, if you do not
+            %   want to work on currentPhysics
+            %   In Comsol internally, a point is created for the physics
+            %   geometry and then linked to the point source that is
+            %   created for the physics
+            if nargin == 2
+                physics = obj.mCurrentPhysics;
+                assert(~isempty(physics), 'No default physics node specified');
+            else
+                assert(isa(physics, 'com.comsol.clientapi.physics.impl.PhysicsClient'), 'Second input must be a Comsol Physics node')
+            end
+            assert( ~obj.IsBoundaryMethod(physics), 'Point sources are not allowed for physics with boundary methods')
+            assert(isa(source, 'itaSource') && isscalar(source), 'Input must be a single itaSource object')
             
-            pointTag = [source.name '_position'];
-            sourceTag = [source.name '_source'];
-            obj.setPoint(obj.mCurrentGeometry, pointName, source.position)
+            geometryNode = obj.mModel.geom(physics.geom);
+            pointTag = [source.name '_pointSourcePosition'];
+            sourceTag = [source.name '_pointSource'];
             
-            if ~itaComsolModel.hasFeature(physics, sourceTag)
+            [~, selectionTag] = obj.createPointWithSelection(geometryNode, pointTag, source.position);
+            
+            if ~itaComsolModel.hasFeatureNode(physics, sourceTag)
                 physics.create(sourceTag, 'FrequencyMonopolePointSource', 0);
             end
             sourceNode = physics.feature(sourceTag);
-            
-            sourceNode.selection.set(pointTag, 1);
-            sourceNode.set('Type', 'UserDefined');
-            sourceNode.set('S', 1);
+            sourceNode.selection.named(selectionTag);
+            %sourceNode.set('Type', 'UserDefined');
+            %sourceNode.set('S', 1);
+            %sourceNode.set('Type', 'Power');
+            %sourceNode.set('P_rms', 3);
+            sourceNode.set('Type', 'Flow');
+            sourceNode.set('Qs', 1);
+        end
+    end
+    methods(Access = private, Static = true)
+    end
+    
+    %% Mesh
+    methods
+        function meshes = Mesh(obj)
+            %Returns all mesh nodes as cell array
+            meshes = obj.getRootElementChildren('mesh');
+        end
+        function mesh = FirstMesh(obj)
+            %Returns the first mesh node
+            mesh = obj.getFirstRootElementChild('mesh');
         end
     end
     
