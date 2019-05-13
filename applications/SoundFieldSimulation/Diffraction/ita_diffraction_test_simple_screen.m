829fd99c58d33c077959f8b513ceedada2af3d79
Daniel Filbert <filbert@ITA.AKUSTIK.RWTH-AACHEN.DE>
2019-05-13 11:23:06 +0200

    test script for direct comparison of the three diffraction filters in case of a screen.

--- applications/SoundFieldSimulation/Diffraction/ita_diffraction_test_simple_screen.m
+++ applications/SoundFieldSimulation/Diffraction/ita_diffraction_test_simple_screen.m
@@ -5,16 +5,17 @@ fs = 44100;
 fftDegree = 12;
 
 % wedges
-n1 = [ 1, 1, 0];
-n2 = [-1, 1, 0];
+n1 = [ 1, 0, 0];
+n2 = [-1, 0, 0];
 apexStart = [0, 1, -4];
 apexEnd   = [0, 1,  4];
 apexLen = norm(apexEnd -apexStart);
 apexDir = (apexEnd -apexStart) / apexLen;
 
 infScreen = itaSemiInfinitePlane(n1, apexStart, apexDir);
-infWedge = itaInfiniteWedge(n1, n2, apexStart);
-finWedge = itaFiniteWedge(n1, n2, apexStart, apexLen);
+finScreen = itaFiniteWedge(n1, n2, apexStart, apexLen);
+finScreen.aperture_direction = apexDir;
+finScreen.aperture_end_point = apexEnd;
 
 % interaction points
 src = [-3, 0, 0];
@@ -30,8 +31,8 @@ diffr_tf_btms = diffr_tf;
 
 %% diffraction
 diffr_tf_maekawa.freqData = ita_diffraction_maekawa(infScreen, src, rcv, diffr_tf_maekawa.freqVector, c);
-diffr_tf_utd.freqData = ita_diffraction_utd(infWedge, src, rcv, diffr_tf_maekawa.freqVector, c);
-diffr_tf_btms.timeData = ita_diffraction_btms(finWedge, src, rcv, fs, diffr_tf_maekawa.nSamples, c);
+diffr_tf_utd.freqData = ita_diffraction_utd(infScreen, src, rcv, diffr_tf_maekawa.freqVector, c);
+diffr_tf_btms.timeData = ita_diffraction_btms(finScreen, src, rcv, fs, diffr_tf_maekawa.nSamples, c);
 
 diffr_tf = ita_merge(diffr_tf_maekawa, diffr_tf_utd, diffr_tf_btms);
 diffr_tf_norm = ita_normalize_spk(diffr_tf, 'allchannels');
