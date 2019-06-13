37cf998b061ee9d4dfd81f1ad737685688c3a93f
Mark Müller-Giebeler <mark.mueller-giebeler@akustik.rwth-aachen.de>
2019-06-13 10:52:13 +0200

    in older Matlab versions only char arrays work as dynamic field names of structs

--- applications/SoundFieldSimulation/Comsol/Import/ita_read_comsol_csv.m
+++ applications/SoundFieldSimulation/Comsol/Import/ita_read_comsol_csv.m
@@ -78,13 +78,13 @@ columnHeaders = char(metaData(end));
 columnHeaders = strsplit(columnHeaders(3:end),',');
 idxExtraParameter = ~contains(columnHeaders, '@') & ~strcmpi(columnHeaders, 'x') & ~strcmpi(columnHeaders, 'y') & ~strcmpi(columnHeaders, 'z');
 nMerges = 0;
-for idxMerge = find(idxExtraParameter)
-    idxSource = idxMerge-nMerges;
-    idxTarget = idxSource-1;
-    columnHeaders{idxTarget} = [columnHeaders{idxTarget} columnHeaders{idxSource}];
-    columnHeaders(idxSource) = [];
-    nMerges = nMerges+1;
-end
+% for idxMerge = find(idxExtraParameter)
+%     idxSource = idxMerge-nMerges;
+%     idxTarget = idxSource-1;
+%     columnHeaders{idxTarget} = [columnHeaders{idxTarget} columnHeaders{idxSource}];
+%     columnHeaders(idxSource) = [];
+%     nMerges = nMerges+1;
+% end
 
 %--Numeric Data---
 try
@@ -190,10 +190,10 @@ end
 %% parse Data
 freqData = numericCsvData(:, colIdxFreqData);
 for iPhysic = 1:numel(physics)
-    currPhysics = string(physics(iPhysic));
+    currPhysics = char(physics(iPhysic));
     
     for iVariable = 1:numel(variables)
-        currVariable = string(variables(iVariable));
+        currVariable = char(variables(iVariable));
         
         if isempty(additionalParameters)
             structOut.(currPhysics).(currVariable) = parseFreqParameterData(freqData, organizedHeaderData, coords, freqVector, currPhysics, currVariable);
