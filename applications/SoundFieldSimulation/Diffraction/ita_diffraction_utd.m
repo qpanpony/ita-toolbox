a3e0c65c194ee478d3960d716c17e98cef32bf3f
Daniel Filbert <filbert@ITA.AKUSTIK.RWTH-AACHEN.DE>
2019-07-04 15:32:23 +0200

    clean up diffraction_utd

--- applications/SoundFieldSimulation/Diffraction/ita_diffraction_utd.m
+++ applications/SoundFieldSimulation/Diffraction/ita_diffraction_utd.m
@@ -11,175 +11,142 @@ function [ diffr_field, D, A ] = ita_diffraction_utd( wedge, source_pos, receive
 %
 
 %% Assertions
-dim_freq = size( frequency_vec );
-dim_src = size( source_pos );
-dim_rcv = size( receiver_pos );
-if dim_freq(1) ~= 1
-    if dim_freq(2) ~= 1
-        error( 'Invalid frequency vector' );
-    end
-    frequency_vec = frequency_vec';
+if ~ita_diffraction_point_is_of_dim3( source_pos )
+    error( 'Source point must be of dimension 3' )
 end
-if dim_src(2) ~= 3
-    if dim_src(1) ~= 3
-        error( 'Source point(s) must be of dimension 3')
-    end
+if ~ita_diffraction_point_is_of_dim3( receiver_pos )
+    error( 'Receiver point must be of dimension 3' )
+end
+if ~ita_diffraction_point_is_row_vector( source_pos )
     source_pos = source_pos';
-    dim_src = size( source_pos );
 end
-if dim_src(1) == 0
-    error( 'no source found' );
-end
-if dim_rcv(2) ~= 3
-    if dim_rcv(1) ~= 3
-        error( 'Receiver point(s) must be of dimension 3')
-    end
+if ~ita_diffraction_point_is_row_vector( receiver_pos )
     receiver_pos = receiver_pos';
-    dim_rcv = size( receiver_pos );
-end
-if dim_rcv(1) == 0
-    error( 'no receiver found' );
-end
-if dim_src(1) ~= 1 && dim_rcv(1) ~= 1 && dim_src(1) ~= dim_rcv(1)
-    error( 'Number of receiver and source positions do not match' )
-end
-switch dim_src(1) >= dim_rcv(1)
-    case true
-        dim_n = dim_src(1);
-    case false
-        dim_n = dim_rcv(1);
 end
 
 %% Variables
-if dim_src(1) == dim_n
-    S = source_pos;
-    if dim_rcv(1) == dim_n
-        R = receiver_pos;
-    else
-        R = repmat( receiver_pos, dim_n, 1 );
-    end
-else
-    R = receiver_pos;
-    if dim_src(1) == dim_n
-        S = source_pos;
-    else
-        S = repmat( source_pos, dim_n, 1 );
-    end
-end
-Apex_Point = wedge.get_aperture_point( source_pos, receiver_pos );
-rho = Norm( Apex_Point - S ); % Distance of source to aperture point
-r = Norm( R - Apex_Point ); % Distance of receiver to aperture point
-c = speed_of_sound;
+apex_point = wedge.get_aperture_point( source_pos, receiver_pos );
+rho = norm( apex_point - source_pos ); % Distance of source to aperture point
+r = norm( receiver_pos - apex_point ); % Distance of receiver to aperture point
+assert( rho + r ~= 0 && r ~= 0 );
 
-face = wedge.point_facing_main_side( S );
-alpha_i = wedge.get_angle_from_point_to_wedge_face( S, face );
-alpha_d = wedge.get_angle_from_point_to_wedge_face( R, face );
-theta_i = wedge.get_angle_from_point_to_aperture( S, Apex_Point );
+src_facing_main_side = wedge.point_facing_main_side( source_pos );
+alpha_i = wedge.get_angle_from_point_to_wedge_face( source_pos, src_facing_main_side );
+alpha_d = wedge.get_angle_from_point_to_wedge_face( receiver_pos, src_facing_main_side );
+theta_i = wedge.get_angle_from_point_to_aperture( source_pos, apex_point );
 
 n = wedge.opening_angle / pi; % Variable dependend on opening angle of the wedge
+k = (2 * pi * frequency_vec) ./ speed_of_sound; % Wavenumber
 
-lambda = c ./ frequency_vec; % Wavelength
-k = (2 * pi) ./ lambda; % Wavenumber
+%% Calculations
+H_i = 1 ./ rho .* exp( -1i * k .* rho ); % direct path from source to apex point
+A = sqrt( rho ./ ( r .* ( rho + r ) ) ); % attenuation factor at receiver
+D = get_diffr_coeff( wedge, k, alpha_d, alpha_i, rho, r, theta_i, n );
 
+% Combined diffracted sound field filter at receiver
+diffr_field = H_i .* D .* A .* exp( -1i .* k .* r );
 
-% Diffraction coefficient D
-assert( all( rho + r ~= 0 ) && all( r ~= 0 )  );
-A = repmat( sqrt( rho ./ ( r .* ( rho + r ) ) ), 1, numel( frequency_vec ) );
-L = repmat( ( ( rho .* r ) ./ ( rho + r ) ) .* ( sin( theta_i ) ).^2, 1, numel( frequency_vec ) );
-H_i = 1 ./ rho .* exp( -1i * k .* rho ); % Consideration of transfer path from source to apex point
+end
 
-D_factor = -exp( -1i * pi / 4 ) ./ ( 2 * n * sqrt( 2* pi * k ) .* sin( theta_i ) );
+function D = get_diffr_coeff( wedge, k, alpha_d, alpha_i, rho, r, theta_i, n )
 
-Cot1 = repmat( cot( ( pi + ( alpha_d - alpha_i ) ) ./ ( 2 * n ) ), 1, numel( frequency_vec ) );
-Cot2 = repmat( cot( ( pi - ( alpha_d - alpha_i ) ) ./ ( 2 * n ) ), 1, numel( frequency_vec ) );
-Cot3 = repmat( cot( ( pi + ( alpha_d + alpha_i ) ) ./ ( 2 * n ) ), 1, numel( frequency_vec ) );
-Cot4 = repmat( cot( ( pi - ( alpha_d + alpha_i ) ) ./ ( 2 * n ) ), 1, numel( frequency_vec ) );
+    L = ( ( rho * r ) ./ ( rho + r ) ) * ( sin( theta_i ) ).^2;
 
-a1 = 2 * ( cos( ( 2 * pi * n * N_p( n, alpha_d - alpha_i ) - ( alpha_d - alpha_i ) ) / 2 ) ).^2;
-a2 = 2 * ( cos( ( 2 * pi * n * N_n( n, alpha_d - alpha_i ) - ( alpha_d - alpha_i ) ) / 2 ) ).^2;
-a3 = 2 * ( cos( ( 2 * pi * n * N_p( n, alpha_d + alpha_i ) - ( alpha_d + alpha_i ) ) / 2 ) ).^2;
-a4 = 2 * ( cos( ( 2 * pi * n * N_n( n, alpha_d + alpha_i ) - ( alpha_d + alpha_i ) ) / 2 ) ).^2;
+    Cot1 = cot( ( pi + ( alpha_d - alpha_i ) ) ./ ( 2 * n ) );
+    Cot2 = cot( ( pi - ( alpha_d - alpha_i ) ) ./ ( 2 * n ) );
+    Cot3 = cot( ( pi + ( alpha_d + alpha_i ) ) ./ ( 2 * n ) );
+    Cot4 = cot( ( pi - ( alpha_d + alpha_i ) ) ./ ( 2 * n ) );
 
-F1 = kawai_approx_fresnel( k .* L .* a1 );
-F2 = kawai_approx_fresnel( k .* L .* a2 );
-F3 = kawai_approx_fresnel( k .* L .* a3 );
-F4 = kawai_approx_fresnel( k .* L .* a4 );
+    a1 = 2 * ( cos( ( 2 * pi * n * N_p( n, alpha_d - alpha_i ) - ( alpha_d - alpha_i ) ) / 2 ) ).^2;
+    a2 = 2 * ( cos( ( 2 * pi * n * N_n( n, alpha_d - alpha_i ) - ( alpha_d - alpha_i ) ) / 2 ) ).^2;
+    a3 = 2 * ( cos( ( 2 * pi * n * N_p( n, alpha_d + alpha_i ) - ( alpha_d + alpha_i ) ) / 2 ) ).^2;
+    a4 = 2 * ( cos( ( 2 * pi * n * N_n( n, alpha_d + alpha_i ) - ( alpha_d + alpha_i ) ) / 2 ) ).^2;
 
-% Avoid eventual singularities of the cot terms at the shadow or reflection boundary with a approximation by
-% Kouyoumjian and Pathak
-mask1 = ( alpha_d - alpha_i ) - 2 * pi * n * N_p( n, alpha_d - alpha_i ) + pi == 0;
-mask2 = - ( alpha_d - alpha_i ) + 2 * pi * n * N_n( n, alpha_d - alpha_i ) + pi == 0;
-mask3 = ( alpha_d + alpha_i ) - 2 * pi * n * N_p( n, alpha_d + alpha_i ) + pi == 0;
-mask4 = - ( alpha_d + alpha_i ) + 2 * pi * n * N_n( n, alpha_d + alpha_i ) + pi == 0;
-  
-singularities = [ any( mask1 ~= 0 ), any( mask2 ~= 0 ), any( mask3 ~= 0 ), any( mask4 ~= 0 ) ];
+    F1 = kawai_approx_fresnel( k .* L .* a1 );
+    F2 = kawai_approx_fresnel( k .* L .* a2 );
+    F3 = kawai_approx_fresnel( k .* L .* a3 );
+    F4 = kawai_approx_fresnel( k .* L .* a4 );
 
+    singularities_present = check_for_singularities( wedge, alpha_i, alpha_d, n );
+    
+    if any( singularities_present )
+        [term1, term2, term3, term4] = approx_for_singularities( n, k, L, alpha_i, alpha_d, singularities_present );
+    else
+        term1 = Cot1 .* F1;
+        term2 = Cot2 .* F2;
+        term3 = Cot3 .* F3;
+        term4 = Cot4 .* F4;
+    end
 
-if any( singularities )
-    if singularities(1)
-        eps1 =   ( alpha_d(mask1) - alpha_i(mask1) ) - 2 * pi * n * N_p( n, alpha_d(mask1) - alpha_i(mask1) ) + pi;
+    if wedge.is_boundary_condition_hard
+        s = 1;
+    else
+        s = -1;
+    end
+    
+    prefactor = -exp( -1i * pi / 4 ) ./ ( 2 * n * sqrt( 2* pi * k ) .* sin( theta_i ) );
+
+    D = prefactor .* ( term1 + term2 + s * ( term3 + term4 ) );
+end
+
+function res = check_for_singularities( wedge, alpha_i, alpha_d, n )
+    % Avoid eventual singularities of the cot terms at the shadow or reflection boundary with a approximation by
+    % Kouyoumjian and Pathak
+    eps = wedge.set_get_geo_eps;
+    is_singular_1 = abs( ( alpha_d - alpha_i ) - 2 * pi * n * N_p( n, alpha_d - alpha_i ) + pi ) < eps;
+    is_singular_2 = abs( - ( alpha_d - alpha_i ) + 2 * pi * n * N_n( n, alpha_d - alpha_i ) + pi ) < eps;
+    is_singular_3 = abs( ( alpha_d + alpha_i ) - 2 * pi * n * N_p( n, alpha_d + alpha_i ) + pi ) < eps;
+    is_singular_4 = abs( - ( alpha_d + alpha_i ) + 2 * pi * n * N_n( n, alpha_d + alpha_i ) + pi ) < eps;
+    res = [is_singular_1, is_singular_2, is_singular_3, is_singular_4];
+end
+
+function [term1, term2, term3, term4] = approx_for_singularities( n, k, L, alpha_i, alpha_d, singularities_present )
+    if singularities_present(1)
+        eps1 =   ( alpha_d - alpha_i ) - 2 * pi * n * N_p( n, alpha_d - alpha_i ) + pi;
     else
         eps1 = 0;
     end
-    if singularities(2)
-        eps2 = - ( alpha_d(mask2) - alpha_i(mask2) ) + 2 * pi * n * N_n( n, alpha_d(mask2) - alpha_i(mask2) ) + pi;
+    if singularities_present(2)
+        eps2 = - ( alpha_d - alpha_i ) + 2 * pi * n * N_n( n, alpha_d - alpha_i ) + pi;
     else
         eps2 = 0;
     end
-    if singularities(3)
-        eps3 =   ( alpha_d(mask3) + alpha_i(mask3) ) - 2 * pi * n * N_p( n, alpha_d(mask3) + alpha_i(mask3) ) + pi;
+    if singularities_present(3)
+        eps3 =   ( alpha_d + alpha_i ) - 2 * pi * n * N_p( n, alpha_d + alpha_i ) + pi;
     else
         eps3 = 0;
     end
-    if singularities(4)
-        eps4 = - ( alpha_d(mask4) + alpha_i(mask4) ) + 2 * pi * n * N_n( n, alpha_d(mask4) + alpha_i(mask4) ) + pi;
+    if singularities_present(4)
+        eps4 = - ( alpha_d + alpha_i ) + 2 * pi * n * N_n( n, alpha_d + alpha_i ) + pi;
     else
         eps4 = 0;
     end
 
-    term1(mask1, :) = n * exp( 1i * pi/4 ) * ( sqrt( 2 * pi .* k .* L(mask1, :) ) .* sgn( eps1 ) - 2 .* k .* L(mask1, :) .* eps1 * exp( 1i * pi/4 ) );
-    term2(mask2, :) = n * exp( 1i * pi/4 ) * ( sqrt( 2 * pi .* k .* L(mask2, :) ) .* sgn( eps2 ) - 2 .* k .* L(mask2, :) .* eps2 * exp( 1i * pi/4 ) );
-    term3(mask3, :) = n * exp( 1i * pi/4 ) * ( sqrt( 2 * pi .* k .* L(mask3, :) ) .* sgn( eps3 ) - 2 .* k .* L(mask3, :) .* eps3 * exp( 1i * pi/4 ) );
-    term4(mask4, :) = n * exp( 1i * pi/4 ) * ( sqrt( 2 * pi .* k .* L(mask4, :) ) .* sgn( eps4 ) - 2 .* k .* L(mask4, :) .* eps4 * exp( 1i * pi/4 ) );
+    term1 = n * exp( 1i * pi/4 ) * ( sqrt( 2 * pi .* k .* L ) .* sgn( eps1 ) - 2 .* k .* L .* eps1 * exp( 1i * pi/4 ) );
+    term2 = n * exp( 1i * pi/4 ) * ( sqrt( 2 * pi .* k .* L ) .* sgn( eps2 ) - 2 .* k .* L .* eps2 * exp( 1i * pi/4 ) );
+    term3 = n * exp( 1i * pi/4 ) * ( sqrt( 2 * pi .* k .* L ) .* sgn( eps3 ) - 2 .* k .* L .* eps3 * exp( 1i * pi/4 ) );
+    term4 = n * exp( 1i * pi/4 ) * ( sqrt( 2 * pi .* k .* L ) .* sgn( eps4 ) - 2 .* k .* L .* eps4 * exp( 1i * pi/4 ) );
 end
-
-term1(~mask1, :) = Cot1(~mask1, :) .* F1(~mask1, :);
-term2(~mask2, :) = Cot2(~mask2, :) .* F2(~mask2, :);
-term3(~mask3, :) = Cot3(~mask3, :) .* F3(~mask3, :);
-term4(~mask4, :) = Cot4(~mask4, :) .* F4(~mask4, :);
-
-if wedge.is_boundary_condition_hard
-    s = 1;
-else
-    s = -1;
-end
-
-D = D_factor .* ( term1 + term2 + s * ( term3 + term4 ) );
-
-% Combined diffracted sound field filter at receiver
-diffr_field = (  H_i .* D .* A .* exp( -1i .* k .* r ) )';
-diffr_field = -conj( diffr_field );
-
-end
-
 
 %% Auxiliary functions
-% euclidean norm row wise
-function res = Norm( A )
-    res = sqrt( sum( A.^2, 2 ) );
-end
-
 % N+ function
 function N = N_p( n, beta )
-    N = zeros( numel( beta ), 1 );
-    N( beta >  pi * ( 1 - n ) ) = 1;
+    if beta > pi * (n - 1)
+        N = 1;
+    else
+        N = 0;
+    end
 end
 
 % N- function
 function N = N_n( n, beta )
-    N = zeros( numel( beta ), 1 );
-    N( beta < pi * ( 1 - n ) ) = -1;
-    N( beta > pi * ( 1 + n ) ) = 1;
+    if beta > pi * (1 + n)
+        N = 1;
+    elseif beta < pi * (n - 1)
+        N = -1;
+    else
+        N = 0;
+    end
 end
 
 % signum function
