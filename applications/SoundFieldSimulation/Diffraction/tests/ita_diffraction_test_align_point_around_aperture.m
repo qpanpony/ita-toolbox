bca36d6c9f4bd6d17438b31526b207c883bef27c
Daniel Filbert <filbert@ITA.AKUSTIK.RWTH-AACHEN.DE>
2019-07-04 15:15:06 +0200

    clean up test functions

---
+++ applications/SoundFieldSimulation/Diffraction/tests/ita_diffraction_test_align_point_around_aperture.m
@@ -0,0 +1,30 @@
+%% Init
+% infite wedge
+inf_wdg = ita_diffraction_create_standard_rectangular_wedge();
+
+% Point
+point_start_pos = [ 1, 0, 0];
+point_end_pos = [-1, 0, 0];
+num_of_positions = 100;
+
+%% Align points around aperture
+apex_point = [0, 0, 0];
+
+point_pos_start_angle = inf_wdg.get_angle_from_point_to_wedge_face(point_start_pos, false);
+point_pos_end_angle = inf_wdg.get_angle_from_point_to_wedge_face(point_end_pos, false);
+point_angles = linspace( point_pos_start_angle, point_pos_end_angle, num_of_positions );
+
+%% Set different receiver positions rotated around the aperture
+aligned_positions = ita_align_points_around_aperture( inf_wdg, point_start_pos, point_angles, apex_point, false );
+
+orig_dist = norm(point_start_pos);
+for i = 1 : size(aligned_positions, 1)
+    assert( point_keeps_same_dist(aligned_positions(i, :), orig_dist, inf_wdg.set_get_geo_eps * 10) )
+end
+disp('Test passed');
+
+%% aux function
+function res = point_keeps_same_dist(point, orig_dist_from_aperture, tolerance)
+    diff = norm(point) - orig_dist_from_aperture;
+    res = abs( diff ) < tolerance;
+end
--- applications/SoundFieldSimulation/Diffraction/tests/ita_diffraction_test_utd_symmetry.m
+++ applications/SoundFieldSimulation/Diffraction/tests/ita_diffraction_test_utd_symmetry.m
@@ -5,12 +5,12 @@ n2 = [ -1, 0, 1 ];
 loc = [ 0 0 0 ];
 w = itaInfiniteWedge( n1 / norm( n1 ), n2 / norm( n2 ), loc );
 
-s = [  3.1 0 -1.4 ];
-r = [ -3.1 0 -1.4 ];
+s = [  3.1 0 0 ];
+r = [ -3.1 0 -0.1 ];
 
 assert( w.point_outside_wedge( s ) )
 assert( w.point_outside_wedge( r ) )
-assert( ita_diffraction_shadow_zone( w, s, r ) )
+%assert( ita_diffraction_shadow_zone( w, s, r ) )
 
 utd_tf = itaAudio( 1 );
 utd_tf.fftDegree = 11;
@@ -19,11 +19,11 @@ f = utd_tf.freqVector( 2:end );
 c = 341;
 
 [ H1, D1, A1 ] = ita_diffraction_utd( w, s, r, f, c );
-utd_tf.freqData( :, 1 ) = [ 0; H1 ];
-utd_tf.freqData( :, 3 ) = [ 0; D1' ];
+utd_tf.freqData( :, 1 ) = [ 0 H1 ];
+utd_tf.freqData( :, 3 ) = [ 0 D1 ];
 [ H2, D2, A2 ] = ita_diffraction_utd( w, r, s, f, c );
-utd_tf.freqData( :, 2) = [ 0; H2 ];
-utd_tf.freqData( :, 4 ) = [ 0; D2' ];
+utd_tf.freqData( :, 2) = [ 0 H2 ];
+utd_tf.freqData( :, 4 ) = [ 0 D2 ];
 
 utd_tf.channelNames = { 'H1', 'H2', 'D1', 'D2' };
 
