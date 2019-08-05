ac5dad5addf55a2ee4e122661ef527d31b34d527
Dipl.-Ing. Jonas Stienen <jst@akustik.rwth-aachen.de>
2019-08-05 09:00:01 +0200

    Refactoring

--- applications/SoundFieldSimulation/Diffraction/@itaInfiniteWedge/point_outside_wedge.m
+++ applications/SoundFieldSimulation/Diffraction/@itaInfiniteWedge/point_outside_wedge.m
@@ -1,11 +1,14 @@
-function bRes = point_outside_wedge( obj, point )
-    % Returns true if point is outside the solid structure of
-    % the infinite wedge
-    
-    assert( ita_diffraction_point_is_of_dim3( point ) );
-        
-    distFromPoint2MainFace = dot( point - obj.location, obj.main_face_normal );
-    distFromPoint2OppositeFace = dot( point - obj.location, obj.opposite_face_normal );
-    
-    bRes = (distFromPoint2MainFace >= -obj.set_get_geo_eps) || (distFromPoint2OppositeFace >= -obj.set_get_geo_eps);
+function point_is_outside = point_outside_wedge( obj, point )
+% point_outside_wedge Returns true if point is outside the solid structure of
+% the infinite wedge
+
+assert( ita_diffraction_point_is_of_dim3( point ) );
+
+distance_main_plane = dot( point - obj.location, obj.main_face_normal );
+distance_opposite_plane = dot( point - obj.location, obj.opposite_face_normal );
+
+spatial_threshold = -obj.set_get_geo_eps * 10;
+point_is_outside = ( distance_main_plane >= spatial_threshold ) || ...
+                   ( distance_opposite_plane >= spatial_threshold );
+
 end
