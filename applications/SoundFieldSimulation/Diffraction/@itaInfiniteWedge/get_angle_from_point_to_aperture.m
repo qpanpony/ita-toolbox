f950c32340d266858121a19d8eb2258388694907
Dipl.-Ing. Jonas Stienen <jst@akustik.rwth-aachen.de>
2019-08-05 08:55:56 +0200

    Fixing problem (self-made) by re-adding acos() to the dot product value of the aperture direction and point-on-aperture

--- applications/SoundFieldSimulation/Diffraction/@itaInfiniteWedge/get_angle_from_point_to_aperture.m
+++ applications/SoundFieldSimulation/Diffraction/@itaInfiniteWedge/get_angle_from_point_to_aperture.m
@@ -3,19 +3,19 @@ function alpha_rad = get_angle_from_point_to_aperture( obj, field_point, point_o
 %aperture of the wedge.
 %   output angle alpha: 0 <= alpha <= pi/2
 
-
 if ~obj.point_outside_wedge( field_point )
     error( 'Field point must be outside wedge' );
 end
+
 if ~obj.point_on_aperture( point_on_aperture )
     error( 'No point on aperture found' )
 end
 
 dir_vec = ( point_on_aperture - field_point ) / norm( point_on_aperture - field_point );
-alpha_rad =dot( dir_vec, obj.aperture_direction );
+alpha_rad = acos( dot( dir_vec, obj.aperture_direction ) );
+
 if alpha_rad > pi/2
     alpha_rad = pi - alpha_rad;
 end
 
 end
-
