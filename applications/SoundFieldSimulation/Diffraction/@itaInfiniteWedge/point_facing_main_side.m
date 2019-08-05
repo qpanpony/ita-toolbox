d7fb3b96dc3197eb9efd45ca2dbce38b8b700a7d
Dipl.-Ing. Jonas Stienen <jst@akustik.rwth-aachen.de>
2019-08-05 09:02:25 +0200

    Adding a warning because this function should not be used at all. Try to avoid geometrically unsymmetric implementations.

--- applications/SoundFieldSimulation/Diffraction/@itaInfiniteWedge/point_facing_main_side.m
+++ applications/SoundFieldSimulation/Diffraction/@itaInfiniteWedge/point_facing_main_side.m
@@ -1,21 +1,11 @@
-function res = point_facing_main_side( obj, point  )
-% Returns string including the face normal of source
-%   facing wedge side
-%   output: 'n1' -> source is facing main face of wedge
-%           'n2' -> source is facing opposite face of wedge
+function res = point_facing_main_side( obj, point )
+% point_facing_main_side Returns true if the point is on the main side
 
 if ~obj.point_outside_wedge( point )
-
     error( 'Source position(s) must be outside the wedge!' );
 end
-%}
-%% begin
-% Coordinate transformation with aperture location as origin
-% e_z = repmat( obj.aperture_direction, dim(1), 1 );
-% e_y1 = repmat( obj.main_face_normal, dim(1), 1 );
-%e_x1 = cross( e_y1, e_z, 2 ); % direction of main face of the wedge
-%e_y2 = repmat( obj.opposite_face_normal, dim(1), 1 );
-%e_x2 = cross( e_z, e_y2, 2 );
+
+warning( 'This function has errors, consider to implement a robust function that does not require this.' )
 
 e_z = obj.aperture_direction;
 e_y1 = obj.main_face_normal;
