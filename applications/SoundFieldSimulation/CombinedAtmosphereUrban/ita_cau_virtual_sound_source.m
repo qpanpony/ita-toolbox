b491bb37315d344734bda48a5fdb7e347ccc9552
Dipl.-Ing. Jonas Stienen <jst@akustik.rwth-aachen.de>
2020-05-14 10:29:32 +0200

    Adding Urban and Atmopsheric model combination

---
+++ applications/SoundFieldSimulation/CombinedAtmosphereUrban/ita_cau_virtual_sound_source.m
@@ -0,0 +1,5 @@
+function [ virtual_source_pos ] = ita_cau_virtual_sound_source( receiver_pos, incident_direction, distance_or_attenuation, modus )
+%%ita_cau_virtual_sound_source Calculates a virtual sound source from an
+%incident direction and distance or spreading loss attenuation.
+
+end
