d2f617d09967095e8649d2650ef035a4b4237d9b
Lukas Aspöck <lukas.aspoeck@rwth-aachen.de>
2020-06-16 11:34:44 +0200

    - added RAVEN shoebox demo script which only simulates a BRIR - adjusted corresponding VA demo script using the shoebox demo

--- applications/VirtualAcoustics/VA/itaVA_experimental_RAVEN.m
+++ applications/VirtualAcoustics/VA/itaVA_experimental_RAVEN.m
@@ -14,11 +14,12 @@ va.set_rendering_module_parameters( mod_id, update_t );
 
 
 %% Now execute RAVEN demo to aquire a BRIR
-ita_raven_demo % will generate BRIR itaAudio object with name 'binaural'
+ita_raven_demo_shoebox_SimOnly % will generate BRIR itaAudio object with name 'binaural'
 
 
 %% Exchange BRIR
-update_t.ch1 = double( binaural.ch( 1 ).timeData )';
-update_t.ch2 = double( binaural.ch( 2 ).timeData )';
-update_t.verbose = true;
-va.set_rendering_module_parameters( mod_id, update_t )
+update_t_RAVEN=update_t;
+update_t_RAVEN.ch1 = double( 0.3*binaural.ch( 1 ).timeData )';
+update_t_RAVEN.ch2 = double( 0.3*binaural.ch( 2 ).timeData )';
+update_t_RAVEN.verbose = true;
+va.set_rendering_module_parameters( mod_id, update_t_RAVEN )
