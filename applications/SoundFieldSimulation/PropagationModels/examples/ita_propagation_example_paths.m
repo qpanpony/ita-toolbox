9fd619b77094c1b29df2a3209ecb63552b9e86d6
Dipl.-Ing. Jonas Stienen <jst@akustik.rwth-aachen.de>
2020-06-18 14:14:53 +0200

    WIP

--- applications/SoundFieldSimulation/PropagationModels/examples/ita_propagation_example_paths.m
+++ applications/SoundFieldSimulation/PropagationModels/examples/ita_propagation_example_paths.m
@@ -10,3 +10,12 @@ pps_2 = ita_propagation_load_paths( 'ppa_example_paths_2.json' );
 [ pps_2, pps_hash_table_2 ] = ita_propagation_paths_add_identifiers( pps_2 );
 
 [ pps_new, pps_del, pps_alt ] = ita_propagation_paths_diff( pps_with_ids, pps_2 );
+
+
+%% Single path transfer function
+ita_propagation_load_defaults
+c = ita_propagation_defaults.air.speed_of_sound;
+
+path_tf = itaAudio();
+path_tf.fftDegree = 16;
+path_tf.freqData = ita_propagation_tf( pps( 13 ), path_tf.samplingRate, path_tf.fftDegree, c );
--- applications/SoundFieldSimulation/PropagationModels/ita_propagation_specular_reflection.m
+++ applications/SoundFieldSimulation/PropagationModels/ita_propagation_specular_reflection.m
@@ -1,7 +1,7 @@
 function [ freq_data_linear ] = ita_propagation_specular_reflection( anchor, source_direction, target_direction, fs, fft_degree )
 %ITA_PROPAGATION_SPECULAR_REFLECTION Returns the attenuation transfer function
 %of the specular reflection e.g. based on reflection factor in frequency domain for a
-%given in/out direction sampling rate and fft degree (defaults to fs = 44100 and fft_degree = 15)
+%given in/out direction, sampling rate and fft degree (defaults to fs = 44100 and fft_degree = 15)
 %
 
 if nargin < 2
@@ -19,9 +19,11 @@ if ~isfield( anchor, 'anchor_type' )
     error( 'The anchor argument does not contain a field "anchor_type"' )
 end
 
-warning( 'ita_propagation_directivity not implemented yet, returning neutral transfer function values' )
+warning( 'ita_propagation_specular_reflection not implemented yet, returning perfectly reflected transfer function values' )
 % @todo generate reflection data set from source_direction and
 % target_direction, if necessary, and return attenuation filter
+
+
 
 specrefl_tf = itaAudio;
 specrefl_tf.samplingRate = fs;
